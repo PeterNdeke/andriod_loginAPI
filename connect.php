5b1c902c6c27a607d339368e7d8dbaecdfe05933
PeterNdeke <35370838+PeterNdeke@users.noreply.github.com>
2018-04-22 18:54:25 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+class connect {
+    private $conn;
+
+    // Connecting to database
+    public function connect() {
+        require_once 'config.php';
+
+        // Connecting to mysql database
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+        // return database object
+        return $this->conn;
+    }
+}
+
+?>
